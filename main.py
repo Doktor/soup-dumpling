9d28a195963365dc4614536da48fd3a097adeada
Doktor <doktorthehusky@gmail.com>
2017-10-14 01:37:49 -0400

    Version 3.0.0

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-VERSION = (2, 0, 0)
+VERSION = (3, 0, 0)
 
 # Truncate long quotes if they contain at least this many characters
 TRUNCATE_LENGTH = 800
