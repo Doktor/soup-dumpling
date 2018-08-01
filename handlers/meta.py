cd6048f3793b5666483ff24c9143e359219d8ba2
Doktor <doktorthehusky@gmail.com>
2018-08-01 03:24:19 -0400

    Specify debug/production mode in 'about' message

--- handlers/meta.py
+++ handlers/meta.py
@@ -1,3 +1,4 @@
+import os
 import shlex
 from subprocess import check_output
 from telegram.ext import CommandHandler, Filters, MessageHandler
@@ -24,35 +25,34 @@ ORIGIN_DIFF = check_output(
 
 PUSHED = COMMIT_HASH not in ORIGIN_DIFF
 
+DEBUG = os.path.isfile('debug')
+
 # The relative date is used for the 'about' command
 DATE_ARGS[4] = '--date=relative'
 
 
 def handle_about(bot, update):
-    if PUSHED:
-        commit = '<a href="{hash_url}">{hash}</a>'
-    else:
-        commit = '{hash}'
+    version = '.'.join((str(n) for n in VERSION))
+    updated_relative = check_output(DATE_ARGS, encoding='utf8')
 
-    info = {
-        'version': '.'.join((str(n) for n in VERSION)),
-        'updated': COMMIT_DATE,
-        'updated_rel': check_output(DATE_ARGS, encoding='utf8'),
-        'repo_url': REPOSITORY_URL,
-        'repo_name': REPOSITORY_NAME,
-        'hash_url': COMMIT_URL,
-        'hash': COMMIT_HASH[:7],
-    }
+    # Add a GitHub link if the current commit exists there
+    if PUSHED:
+        commit = f'<a href="{COMMIT_URL}">{COMMIT_HASH[:7]}</a>'
+    else:
+        commit = f'{COMMIT_HASH[:7]}'
+
+    mode = 'debug' if DEBUG else 'production'
 
     response = [
-        '"Nice quote!" - <b>Soup Dumpling {version}</b>',
-        '<i>{updated} ({updated_rel})</i>',
+        f'"Nice quote!" - <b>Soup Dumpling {version}</b>',
+        f'<i>{COMMIT_DATE} ({updated_relative})</i>',
         '',
-        'Source code at <a href="{repo_url}">{repo_name}</a>',
-        'Running on commit ' + commit,
+        f'Source code at <a href="{REPOSITORY_URL}">{REPOSITORY_NAME}</a>',
+        f'Running on commit {commit}',
+        f'Running in {mode} mode',
     ]
 
-    response = '\n'.join(response).format(**info)
+    response = '\n'.join(response)
 
     update.message.reply_text(response,
         quote=False, disable_web_page_preview=True, parse_mode='HTML')
