044edef1f7787c0cf3e34b85692b943bb4d59887
Doktor <doktorthehusky@gmail.com>
2018-08-03 00:34:27 -0400

    Remove 'author' command; replaced by advanced search tag

--- handlers/quotes.py
+++ handlers/quotes.py
@@ -117,44 +117,6 @@ def handle_vote(bot, update, user_data):
 handler_vote = CallbackQueryHandler(handle_vote, pass_user_data=True)
 
 
-
-def handle_author(bot, update, args=list(), user_data=None):
-    if user_data is None:
-        chat_id = update.message.chat_id
-    else:
-        chat_id = user_data['current']
-
-    if not args:
-        return
-    else:
-        args = ' '.join(args)
-
-    user = update.message.from_user
-    result = database.get_random_quote(chat_id, name=args)
-
-    if len(args) > TRUNCATE_ARGS_LENGTH:
-        args = args[:TRUNCATE_ARGS_LENGTH] + '...'
-
-    if result is None:
-        response = 'no quotes found by author "{}"'.format(escape(args))
-        update.message.reply_text(response)
-    else:
-        votes = create_vote_buttons(
-            user.id, result.quote.id, direct=user_data is not None)
-
-        response = format_quote(*result)
-        message = update.message.reply_text(
-            response, parse_mode='HTML', reply_markup=votes)
-
-        database.add_message(chat_id, message.message_id, result.quote.id)
-
-
-handler_author = CommandHandler(
-    'author', handle_author, filters=Filters.group, pass_args=True)
-dm_handler_author = CommandHandler(
-    'author', handle_author, pass_args=True, **dm_kwargs)
-
-
 def handle_count(bot, update, args=list(), user_data=None):
     if user_data is None:
         chat_id = update.message.chat_id
